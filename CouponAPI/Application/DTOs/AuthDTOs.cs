fbec2bd3517bacbefcd9bffcc08abc9b7060b856
ajithkumarghub <ajithkumar.t2196@gmail.com>
2025-04-14 16:57:00 +0530

    changesadded

--- CouponAPI/Application/DTOs/AuthDTOs.cs
+++ CouponAPI/Application/DTOs/AuthDTOs.cs
@@ -1,6 +1,6 @@
 ﻿namespace CouponAPI.Application.DTOs;
 
-public record LoginRequestDTO(string UserName, string Password);
-public record RegisterationRequestDTO(string UserName, string Name, string Password);
-public record UserDTO(int Id, string UserName, string Name);
+public record LoginRequestDTO(string Email, string Password);
+public record RegisterationRequestDTO(string Email, string Name, string Password);
+public record UserDTO(int Id, string Email, string Name);
 public record LoginResponseDTO(UserDTO User, string Token);
